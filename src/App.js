e1fc13a5a63f7a5acd19ad6d1c61fd4ae57c607c
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-06-16 23:18:12 +0100

    render restaurant component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Bookings from "./Bookings";
 import "./App.css";
 import Heading from "./Header";
+import Restaurant from "./Restaurant";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
-
       <Bookings />
+      <Restaurant />
     </div>
   );
 };
