d1b0bc848d0549db1ca0f1cfa1560f9027727c96
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-06-18 22:33:06 +0100

    working on rest of lesson 1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Bookings from "./Bookings";
 import "./App.css";
 import Heading from "./Header";
 import Restaurant from "./Restaurant";
+import TouristInfoCards from "./TouristInfoCards";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards />
       <Bookings />
       <Restaurant />
     </div>
---
+++ src/TouristInfoCards.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+const TouristInfoCards = () => {
+  return (
+    <div class="card-group">
+      <div className="card">
+        <img
+          src="https://lh3.googleusercontent.com/bip/AKnpU_yX5MS-wJh6EBzyVxm5TmokLtUfuB1aK-IyizrD8xw1KZMvVYraTI5xJyjF7Pjd4RoEUCCU8cD4FKkjpAvQ9K5meJOVznPJtMt_lIUme2usmhextehZ6Y8tYW9sEVpREXFJ5tYhpY7Sm-ITzNbfRj9aPZD-cIsJm-cWQndpWfiVLzRVkyss7ODfVQ_ZnvM=w250-h200-p"
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <h3>Glasgow</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <a href="https://peoplemakeglasgow.com/" className="btn btn-primary">
+            More Information
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://lh3.googleusercontent.com/bip/AKnpU_yTz1TkOJxNnFPE6bxCc0zOWt7XvsdsPnWbiLrgK340CvuTFh7fU1O-XNvweQ2B6b96BdYmESU4RhdYBlQpK8twQLwe00vEvzW7B9fZHv01Mroy67OAd9dd1mB9L-HDMQ2jVbhRFkevV5vQ2bo8IkDrgmuxtp6CAw=w250-h200-p"
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <h3>Manchester</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Malesuada proin libero nunc consequat. Etiam dignissim diam quis
+            enim lobortis scelerisque fermentum. Non odio euismod lacinia at
+            quis. Id venenatis a condimentum vitae. Lorem ipsum dolor sit amet
+            consectetur adipiscing elit pellentesque. Est velit egestas dui id
+            ornare arcu odio ut sem.
+          </p>
+          <a href="http://visitmanchester.com/" className="btn btn-primary">
+            More Information
+          </a>
+        </div>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://lh3.googleusercontent.com/bip/AKnpU_wG_jXZfCirAXvNxP3RD1lDQvToWwmbMpoEueFc3ZH_LH4JMOsPC7RoTLlVA_nJhUm5YxaYXiAQdpXNs6aMFns-kU8xLVUuFOdQSdt-VXEQ1XwXsPldAQJChN4SOpOB5jYijVa16xIT9rdC0tfZxq_QGLzC1wy5WzpC5FccgrPkuF99OBUpdO8lWyAEimkitw3Q-3eyEaACUtbwYonXtJ70ZVufJ8YlSqy1I4YK7MQ71lNtUmWIQz1IG5M=w250-h200-p"
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <h3>London</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <a href="http://visitlondon.com" className="btn btn-primary">
+            More Information
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TouristInfoCards;
